4a86531201ba034c0d6c3c4e0c1d03db77bcda71
Armand Caesar <armand.caesar@gmail.com>
2018-06-26 18:18:18 +0700

    init, add basic configuration and test

---
+++ test/configuration_test.go
@@ -0,0 +1,34 @@
+package test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/Friend-Management/config"
+	"github.com/golang/glog"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReadConfigurationFromFile(t *testing.T) {
+	cfg, err := config.New()
+
+	if err != nil {
+		glog.Fatalf("Failed to load configuration: %s", err)
+		panic(fmt.Errorf("Fatal error on load configuration : %s ", err))
+	}
+
+	assert.Empty(t, err)
+
+	configuration := *cfg
+
+	// test to configuration object
+	assert.NotEmpty(t, configuration)
+
+	//test value
+	assert.Equal(t, configuration.Server.Mode, "debug")
+	assert.Equal(t, configuration.Server.Addr, ":8080")
+	assert.Equal(t, configuration.Server.LogDuration, 3)
+	assert.Equal(t, configuration.Server.ShutdownTimeout, 5)
+	assert.Equal(t, configuration.Server.BaseURL, "http://localhost:8080")
+
+}
