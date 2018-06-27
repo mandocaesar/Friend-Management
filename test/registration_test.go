51eff64fcd92a956473abfa6333b7ee349d62f57
Armand Caesar <armand.caesar@gmail.com>
2018-06-27 12:56:40 +0700

    WIP test

--- test/registration_test.go
+++ test/registration_test.go
@@ -1,8 +1,34 @@
 package test
 
-import "testing"
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/Friend-Management/shared"
+	"github.com/Friend-Management/shared/config"
+	"github.com/golang/glog"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestRegisterUser(t *testing.T) {
 
-	payload := []byte(`{"email":"test@user.com"}`)
+	cfg, err := config.New()
+
+	if err != nil {
+		glog.Fatalf("Failed to load configuration: %s", err)
+		panic(fmt.Errorf("Fatal error on load configuration : %s ", err))
+	}
+
+	assert.Empty(t, err)
+
+	payload := bytes.NewBuffer([]byte(`{"email":"test@user.com"}`))
+
+	router := shared.SetupRouter(cfg)
+
+	//Perform http request
+	response := DispatchRequest(router, "POST", "/v1/api/register", payload)
+
+	assert.Equal(t, http.StatusCreated, response.Code)
 }
